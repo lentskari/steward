cc44948bd771378f51cf55798beb7b2dfe2e60bf
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2015-11-08 00:24:20 +0200

    Airport information to endpoint

--- app/controllers/api/journeys_controller.rb
+++ app/controllers/api/journeys_controller.rb
@@ -6,9 +6,16 @@ class Api::JourneysController < ApplicationController
     current_address = Geocode.from_latlng(lat, lon).full_address
     Rails.logger.info(params)
     first_flight = customer.flights.first
+    origin_airport = Airport.find_by_code(first_flight.origin)
+    destination_airport = Airport.find_by_code(first_flight.destination)
+    flight_duration = first_flight.arrival_time - first_flight.departure_time
     render json: {
       current_address: current_address,
-      next_flight: {
+      next: {
+        flight: first_flight,
+        origin_airport: origin_airport,
+        destination_airport: destination_airport,
+        flight_duration: flight_duration
       }
     }
   end
