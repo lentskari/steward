be70997c4bec7f08bb0ac36afbb3ec8122aa047f
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2015-11-07 23:23:02 +0200

    Reverse geocode

--- app/lib/geocode.rb
+++ app/lib/geocode.rb
@@ -2,4 +2,8 @@ class Geocode
   def self.address(address)
     Geokit::Geocoders::GoogleGeocoder.geocode address
   end
+
+  def self.from_latlng(lat, lng)
+    Geokit::Geocoders::GoogleGeocoder.reverse_geocode("#{lat},#{lng}")
+  end
 end
