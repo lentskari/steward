23b42daa201f0ad03aee9dcb664df8a297188a84
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2015-11-07 12:49:12 +0200

    Add request ride

--- app/lib/uber_api.rb
+++ app/lib/uber_api.rb
@@ -1,7 +1,26 @@
+class NoUberAvailableError < StandardError
+end
+
 class UberAPI
   def products(latitude, longitude)
     response = connection.get("products", latitude: latitude, longitude: longitude)
-    JSON.parse(response.body)
+    JSON.parse(response.body)["products"]
+  end
+
+  def request_ride(customer, locations)
+    first_product = products(locations[:start_lat], locations[:start_lon]).first
+    if first_product.nil?
+      raise NoUberAvailableError
+    end
+    puts first_product["product_id"]
+    response = sandbox_connection(customer).post("requests", {
+      product_id: first_product["product_id"],
+      start_latitude: locations[:start_lat],
+      start_longitude: locations[:start_lon],
+      end_latitude: locations[:end_lat],
+      end_longitude: locations[:end_lon]
+    }.to_json)
+    puts response.body
   end
 
   def authenticate(authorization_code)
@@ -25,9 +44,18 @@ class UberAPI
   private
 
   def connection
-    connection ||= Faraday.new(url: "https://api.uber.com/v1/",
+    @connection ||= Faraday.new(url: "https://api.uber.com/v1/",
                            headers: {
                              Authorization: "Token #{ENV["UBER_SERVER_TOKEN"]}"
                            })
+  end
+
+  def sandbox_connection(customer)
+    @sanbox_connection ||= Faraday.new(url: "https://sandbox-api.uber.com/v1/",
+      headers: {
+        :Authorization => "Bearer #{customer.uber_access_token}",
+        :"Content-Type" => "application/json",
+        :Accept => "application/json"
+      })
   end
 end
